134b12ff57ce26642b0feefa45a592efcf3ad13f
otieno0928 <p.otieno1@alustudent.com>
2026-06-25 13:03:42 -0700

    Fix task 8: Adjust slice index to eliminate duplicate space

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"  # noqa: E501
-str1 = str[39:67]
+str1 = str[39:66]
 str2 = str[107:111]
 print(str1, str2, str[:6])
