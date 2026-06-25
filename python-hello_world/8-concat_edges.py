23366357e8cc83d7a1e4ab588935a002cafc8600
otieno0928 <p.otieno1@alustudent.com>
2026-06-25 12:58:08 -0700

    add task 8 after eliminating the string literals

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-str = (f"{str[39:67]} {str[106:110]} "
-       f"{str[:6]}")
-print(str)
+str1 = str[39:67]
+str2 = str[106:110]
+print(str1, str2, str[:6])
