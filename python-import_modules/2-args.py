93e9f211ebc6ef71848eb5ba5f30018b78522542
otieno0928 <p.otieno1@alustudent.com>
2026-06-25 14:53:39 -0700

    Fix: Add 2-args.py to dynamically print CLI arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    # sys.argv[0] is the script name, so subtract 1 for the actual arguments count
+    count = len(sys.argv) - 1
+
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+
+    # Loop through arguments starting from index 1
+    for i in range(1, count + 1):
+        print("{}: {}".format(i, sys.argv[i]))
