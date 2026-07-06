6948634eaf0d1b3fd5cad5bc30dacb794328dd51
otieno0928 <p.otieno1@alustudent.com>
2026-07-06 15:16:13 -0700

    Add solution for task 6 raise exception message

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module containing a function that raises a NameError with a message."""
+
+
+def raise_exception_msg(message=""):
+    """Raises a NameError with a specific message string."""
+    raise NameError(message)
