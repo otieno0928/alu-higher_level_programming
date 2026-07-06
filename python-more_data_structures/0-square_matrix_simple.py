43a0ce2c1f485a97ccfcda95d1a31a65bc4e765e
otieno0928 <p.otieno1@alustudent.com>
2026-07-06 08:03:28 -0700

    Fix PEP8 styling and add missing docstrings for task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Module that computes the square value of all integers of a matrix."""
+
+
 def square_matrix_simple(matrix=[]):
+    """Computes the square value of all integers of a matrix."""
     return [[x**2 for x in row] for row in matrix]
