13b754524837d9645289831af188c4d5f727cbbc
otieno0928 <p.otieno1@alustudent.com>
2026-07-06 08:17:05 -0700

    Add  tasks 6 and 7

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that replaces or adds key/value in a dictionary."""
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Replaces or adds key/value in a dictionary."""
+    a_dictionary[key] = value
+    return a_dictionary
