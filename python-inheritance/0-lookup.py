f3d8043349f29bf7ac9aaee8b49e2fd695a5d964
otieno0928 <p.otieno1@alustudent.com>
2026-07-25 14:59:40 -0700

    Fix README.md and Task 0 lookup function

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""Module that contains lookup function."""
+"""
+Module that contains the lookup function.
+"""
 
 
 def lookup(obj):
-    """Returns the list of available attributes and methods of an object."""
+    """
+    Returns the list of available attributes and methods of an object.
+    """
     return dir(obj)
