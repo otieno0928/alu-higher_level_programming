ecb5aa0afb2e6b70c1b4dd1032a6db352333a3a0
otieno0928 <p.otieno1@alustudent.com>
2026-07-31 09:08:15 -0700

    Fix header parameter evaluation for task 4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a given URL with a header variable
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request to a URL with a header variable
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
