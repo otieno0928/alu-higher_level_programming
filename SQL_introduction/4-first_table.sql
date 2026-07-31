d703b5abdc684cf3a11eb1bb0f1f9b947da1721d
otieno0928 <p.otieno1@alustudent.com>
2026-07-31 07:53:51 -0700

    Fix README presence and add script header comments

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,1 +1,5 @@
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+-- Creates a table called first_table in the current database
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
