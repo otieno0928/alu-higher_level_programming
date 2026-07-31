5a04bf57ac4d73f52abf19e9f32cd28545021c29
otieno0928 <p.otieno1@alustudent.com>
2026-07-31 08:55:24 -0700

    4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# Sends a GET request to the URL with header X-HolbertonSchool-User-Id: 98
-curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -H "X-HolbertonSchool-User-Id: 98" "$1"
