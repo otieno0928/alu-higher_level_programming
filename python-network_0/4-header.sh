3a6441c385243d15f88f779ea998de4ccf9c255e
otieno0928 <p.otieno1@alustudent.com>
2026-07-31 08:49:58 -0700

    Fix task 4 by explicitly specifying GET request method

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to the URL with header X-HolbertonSchool-User-Id: 98
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request to the URL with a header parameter
+curl -sG -H "X-HolbertonSchool-User-Id: 98" "$1"
